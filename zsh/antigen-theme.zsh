7269ef84c5021caac9db1377971078aee788e8a7
d1rk <d1rk@gmx.de>
2013-04-08 12:23:20 +0200

    add antigen-theme file at zsh

---
+++ zsh/antigen-theme.zsh
@@ -0,0 +1,11 @@
+DEFAULT_USER=d1rk
+POWERLINE_GIT_CLEAN="✔"
+POWERLINE_GIT_DIRTY="✘"
+POWERLINE_GIT_ADDED="%F{green}✚%F{black}"
+POWERLINE_GIT_MODIFIED="%F{blue}✹%F{black}"
+POWERLINE_GIT_DELETED="%F{red}✖%F{black}"
+POWERLINE_GIT_UNTRACKED="%F{yellow}✭%F{black}"
+POWERLINE_GIT_RENAMED="➜"
+POWERLINE_GIT_UNMERGED="═"
+# antigen-theme robbyrussell
+antigen-theme agnoster
