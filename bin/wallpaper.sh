5b7d47abb1a133c9d08bf049326c4e6e471885b5
d1rk <d1rk@gmx.de>
2013-09-06 09:56:45 +0200

    make wallpaper-script less verbose, adding pure.zsh

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -3,7 +3,7 @@ cd ~/Downloads
 while [ 1 ]; do
     COUNTER=0
     while [ $COUNTER -lt 60 ]; do
-        wget http://www.opentopia.com/images/cams/world_sunlight_map_rectangular.jpg -O world.jpg
+        wget -q http://www.opentopia.com/images/cams/world_sunlight_map_rectangular.jpg -O world.jpg
         eval $(stat -s world.jpg)
         if [[ $st_size > 1000 ]]; then
             cp -f world.jpg world_sunlight_Wallpaper.jpg
